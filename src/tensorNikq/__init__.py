53985a19d655a0972a4a8e947a8ac4d3e4fad7a0
19406 <hoaikhiem2k4@gmail.com>
2026-03-13 19:42:38 +0700

    Refactor: Reorganize project structure for proper Python packaging

---
+++ src/tensorNikq/__init__.py
@@ -0,0 +1,24 @@
+"""
+    TENSOR LAYER
+
+    Input : Tensor
+    Output: Tensor    
+"""
+
+from .tensor import Tensor
+from .data_ops import *
+from .function_ops import *
+
+__all__ = [
+    "Tensor",
+    "elementwise_neg", "elementwise_square", "elementwise_sqrt", "elementwise_exp",
+    "elementwise_add", "elementwise_sub", "elementwise_mul", "elementwise_div",
+    "elementwise_transpose_2D", "transpose_last2", "transpose_nd", "elementwise_reshape",
+    "infer_matmul_shape", "elementwise_matmul", "outer",
+    "broadcast_shape", "infer_batch_matmul_shape", "broadcast_to",
+    "sum_dim", "shape_after_sum", "reduce_to_shape",
+    "Neg", "Sum",
+    "Add", "Sub", "Mul", "Div",
+    "Transpose", "Reshape",
+    "MatMul"
+]
